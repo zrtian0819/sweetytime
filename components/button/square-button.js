e3651c63ea0f72f06d2322f26e675359b398f30f
Joe_Zhang <cjt3399@gmail.com>
2024-10-15 01:04:53 +0800

    feat(product):製作商品卡片元件和部分全局樣式定義

---
+++ components/button/square-button.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from 'react'
+
+export default function SquareButton({
+  bgColor = '#f46b63',
+  color = '#ffffff',
+  value = 'button',
+}) {
+  return (
+    <>
+      <div className="ZRT-button">{value}</div>
+
+      <style jsx>
+        {`
+          .ZRT-button {
+            display: inline-flex;
+            justify-content: center;
+            align-items: center;
+            background-color: ${bgColor};
+            color: ${color};
+            padding: 20px 30px;
+          }
+        `}
+      </style>
+    </>
+  )
+}
--- components/product-card/index.js
+++ components/product-card/index.js
@@ -2,11 +2,17 @@ import React, { useState, useEffect } from 'react'
 import styles from './product-card.module.css'
 import Image from 'next/image'
 
-export default function ProductCard({ src }) {
+export default function ProductCard({
+  src = '',
+  name = '產品名稱',
+  price = 200,
+  discount = 1,
+}) {
   return (
     <>
       <div className={styles['ProductCard']}>
         <div className={styles['imgBox']}>
+          <div className={styles['heart']}>❤️</div>
           <Image
             src="/34_cupostory_tart_mushroom.jpg"
             alt={''}
@@ -14,7 +20,24 @@ export default function ProductCard({ src }) {
             height={0}
           />
         </div>
-        <div className={styles['textBox']}></div>
+        <div className={styles['textBox']}>
+          <div className="detail">
+            <div className={styles['productName']}>{name}</div>
+            <div className="productPrice">
+              $
+              {discount === 1 ? (
+                <span className={styles['price']}>{price}</span>
+              ) : (
+                <span>
+                  <del>{price}</del>{' '}
+                  <span className={styles['price']}>{price * discount}</span>
+                </span>
+              )}
+            </div>
+          </div>
+
+          <button className="btn btn-pnk">加入購物車</button>
+        </div>
       </div>
     </>
   )
