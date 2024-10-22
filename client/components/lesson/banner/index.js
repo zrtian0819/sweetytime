ef080320a105c0bad3a506fdca07299def56da29
Tzu <jf76501@gmail.com>
2024-10-22 16:41:57 +0800

    feat(lesson)Banner排版

--- client/components/lesson/banner/index.js
+++ client/components/lesson/banner/index.js
@@ -1,10 +1,34 @@
 import React, { useState, useEffect } from 'react';
+import Styles from './index.module.scss';
 import Image from 'next/image';
 
-export default function Banner() {
+export default function Banner(props) {
 	return (
 		<>
-			<Image src={'/photos/background/bg-section3.png'} width={1400} height={400} alt="bg" />
+			<div className={Styles['CTH-banner']}>
+				<div className={Styles['banner-left']}>
+					<Image
+						src={'/photos/lesson/00_cake_chestnut.png'}
+						width={350}
+						height={300}
+						alt=""
+						className={Styles['image']}
+					/>
+				</div>
+				<div className={Styles['banner-right']}>
+					<div>本月精選課程</div>
+					<div className={Styles['content']}>
+						<div>蒙布朗栗子蛋糕</div>
+						<button>我要報名</button>
+					</div>
+				</div>
+			</div>
+			<div className={Styles['switch']}>
+				<div className={Styles['icon']}>🍞</div>
+				<div className={Styles['icon']}>🥞</div>
+				<div className={Styles['icon']}>🥨</div>
+				<div className={Styles['icon']}>🍫</div>
+			</div>
 		</>
 	);
 }
