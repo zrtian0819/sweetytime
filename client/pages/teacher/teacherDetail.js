d241732bc9d2b98e2bca2e9c2b1fec01656960da
SU0119SU <suyamaomao@gmail.com>
2024-10-23 12:56:44 +0800

    feat(teacher):新增排版進度

--- client/pages/teacher/teacherDetail.js
+++ client/pages/teacher/teacherDetail.js
@@ -2,41 +2,42 @@ import React from 'react';
 import Header from '@/components/header'; 
 import Footer from '@/components/footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '@/styles/teacherDetail.module.scss'; 
-const teacherDetail = () => {
+import styles from '@/styles/teacherDetail.module.scss';  // 正確導入模塊化 SCSS
+
+const TeacherDetail = () => {
   return (
     <>
       <Header />
-    <div className="teacher-detail">
-      <div className="container">
-        <div className="row justify-content-center">
-          {/* 圖片部分 */}
-          <div className="col-12 col-md-6 text-center">
-            <img
-              src="/photos/teachers/maggie.png"
-              alt="Maggie 王美姬"
-              className="teacher-image"
-            />
-          </div>
-          {/* 文字描述部分 */}
-          <div className="col-12 col-md-6 teacher-info">
-            <div className="background-overlay">
-              <h2 className="teacher-title">Maggie 王美姬</h2>
-              <p className="teacher-description">
-                出生於肉類主宰平原的王美姬老師：在春糕技術的環境中長大，傳承正宗的既有手藝。這樣長時間投入美學的心血，為我們打造出一波又一波的熱潮。
-              </p>
-              <p className="teacher-description">
-                2021年榮獲"Mom's Best"排名第一。更帶來創新的"花生牛奶甜點"以及更多。
-              </p>
-              {/* 其他描述文字 */}
+      <div className={styles.teacherDetail}> {/* 使用 styles 來引用類名 */}
+        <div className="container">
+          <div className="row justify-content-center">
+            {/* 圖片部分 */}
+            <div className={`col-12 col-md-6 text-center ${styles.teacherImageBox}`}>
+              <img
+                src="/photos/teachers/maggie.png"
+                alt="Maggie 王美姬"
+                className={styles.teacherImage}
+              />
             </div>
+            </div>
+            </div>
+
+            {/* 文字描述部分 */}
+            <div className={`col-12 col-md-6 ${styles.teacherInfo}`}>
+              <div className={styles.backgroundOverlay}>
+                <h2 className={styles.teacherTitle}>Maggie 王美姬</h2>
+                <p className={styles.teacherDescription}>
+                  出生於肉類主宰平原的王美姬老師：在春糕技術的環境中長大，傳承正宗的既有手藝。這樣長時間投入美學的心血，為我們打造出一波又一波的熱潮。
+                </p>
+                <p className={styles.teacherDescription}>
+                  2021年榮獲"Mom's Best"排名第一。更帶來創新的"花生牛奶甜點"以及更多。
+                </p>
           </div>
         </div>
       </div>
-    </div>
-    <Footer />
+      <Footer />
     </>
   );
 };
 
-export default TeacherPage;
+export default TeacherDetail;
