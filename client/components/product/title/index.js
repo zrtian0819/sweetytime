60d9f8e2f63ebdcdb515769955e82985d23703cc
Peng <tmpss94319@gmail.com>
2024-10-24 11:21:26 +0800

    修改商品細節頁

---
+++ client/components/product/title/index.js
@@ -0,0 +1,10 @@
+// import React, { useState, useEffect } from 'react';
+import Styles from './index.module.scss';
+
+export default function Title() {
+	return (
+		<>
+			<p className={Styles['title']}>Intro</p>
+		</>
+	);
+}
--- client/pages/product/details/index.js
+++ client/pages/product/details/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import Header from '@/components/header';
 import Footer from '@/components/footer';
+import Title from '@/components/product/title';
 import Styles from '@/styles/productDetail.module.scss';
 
-export default function Lesson() {
+export default function Product() {
 	return (
 		<>
 			<Header />
-				<div className={Styles['product ZRT-center']}></div>
+				<div className={`${Styles['product']} ZRT-center`}>
+
+				</div>
+				<div className={`${Styles['introduction']}`}>
+					<Title />
+                </div>
 			<Footer />
 		</>
 	);
