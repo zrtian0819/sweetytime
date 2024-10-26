5b4d1937a266c7bea03e42b2ba6a005551d104a1
SU0119SU <suyamaomao@gmail.com>
2024-10-26 18:10:25 +0800

    feat(teacher)Detail頁新增返回鈕/新增卡片及按鈕狀態

--- client/components/pagination/index.js
+++ client/components/pagination/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './pagination.module.sscss';
+import styles from './pagination.module.scss';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
 const Pagination = ({ currentPage, totalPages, onPageChange, changeColor = 'white' }) => {
