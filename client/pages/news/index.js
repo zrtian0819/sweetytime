1916e0819d4e12724437ebe6e3f27f2be28c0b5c
Salmon-0103 <absolute.still@gmail.com>
2024-10-27 05:07:04 +0800

    refactor(news):調整card & sidebar外觀以及程式碼結構

--- client/pages/news/index.js
+++ client/pages/news/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import Header from '@/components/header';
 import Banner from '@/components/news/banner';
 import NewsCard from '@/components/news/newsCard';
-import sidebar from '@/components/news/sidebar';
+import Sidebar from '@/components/news/sidebar';
 import Footer from '@/components/footer';
-// import styles from '@/styles/news.module.scss';
+import styles from '@/styles/news.module.scss';
+import Pagination from '@/components/pagination';
 
 export default function News() {
 	return (
@@ -12,25 +13,35 @@ export default function News() {
 			<Header />
 			<Banner />
 			<div className="container mt-2">
-				<div></div>
-				<div className='row justify-content-between'></div>
-				<div className="card-group d-flex flex-wrap col-sm-9 justify-content-around">
-				<NewsCard />
-				<NewsCard />
-				<NewsCard />
-				<NewsCard />
-				<NewsCard />
-				<NewsCard />
-				</div>
-				<div>
+				<div className='row justify-content-between'>
+					<div className="news-card-group d-flex flex-wrap col-sm-9 col-md-8 justify-content-around">
+						<NewsCard />
+						<NewsCard />
+						<NewsCard />
+						<NewsCard />
+						<NewsCard />
+						<NewsCard />
+					</div>
 
+					<div className={`${styles['LYT-sm-news-box']} col-auto`}>
+						<div className="text-center mb-3">
+							<Sidebar />
+							<Sidebar />
+							<Sidebar />
+							<Sidebar />
+						</div>
+					</div>
 				</div>
-				
+
+				<div className='mb-3'>
+					<Pagination
+						currentPage={1}
+						totalPages={5}
+						onPageChange={() => { }}
+						changeColor="#fe6f67"
+					/>
+				</div>
 			</div>
-			<div>
-
-      </div>
-
 			<Footer />
 		</>
 	);
