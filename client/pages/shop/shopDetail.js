1967a455dfd2ddc239e373dfe05b55feed6ef0a8
Jolin <lonely507487@gmail.com>
2024-10-28 14:47:01 +0800

    refactor[shop]修整細節頁(sideBar、hover大小)

--- client/pages/shop/shopDetail.js
+++ client/pages/shop/shopDetail.js
@@ -11,7 +11,6 @@ export default function ShopDetail() {
 		'/photos/shop_logo/mpapa_logo.png',
 		'/photos/shop_logo/SEASON_Artisan_Pâtissier_logo.jpg',
 		'/photos/shop_logo/laviebonbon_logo.jpg',
-		'/photos/shop_logo/Gustave_Henri_logo.jpg',
 	];
 
 	const shop = {
@@ -26,76 +25,78 @@ export default function ShopDetail() {
 	return (
 		<>
 			<Header />
-			<div
-				className={`${styles['TIL-shopDetailALL']} container-fluid d-flex flex-md-row flex-column-reverse p-0`}
-			>
-				{/* Sidebar for Circle Logos */}
-				<div className="row col-12 col-md-3 col-xl-3 d-flex flex-row flex-md-column gap-2 mx-auto my-5 position-md-fixed">
-					{circleLogos.map((logo, index) => (
-						<div key={index} className={`${styles['TIL-sideBarCircle']} mx-auto p-0`}>
-							<Image
-								src={logo}
-								alt={`Logo ${index + 1}`}
-								width={70}
-								height={70}
-								className={styles['TIL-sideBarImage']}
-							/>
-						</div>
-					))}
-				</div>
-
-				{/* Shop Details Section */}
-				<div className="col-md-9 col-xl-9 mx-auto me-lg-0 my-5 flex-column d-flex me-0 gap-5">
-					<h1 className="p-lg-5 p-md-3 px-3 text-white">{shop.name}</h1>
-					<div className="d-flex flex-md-row justify-content-center flex-column-reverse px-md-3 px-lg-5">
-						<div
-							className={`${styles['TIL-shop-left']} col-lg-8 col-md-8 mx-lg-5 d-flex flex-column pe-md-2 me-md-3 col-12 p-3 p-lg-0`}
-						>
-							<div className="d-flex flex-row row me-lg-5 gap-3 gap-md-0">
-								<div className="col-lg-3 col-md-4 m-0 text-center">
-									<h3>聯絡電話：</h3>
+			<div className={`${styles['TIL-bgColor']} container-fluid p-0`}>
+				<div className={`${styles['TIL-shopDetailALL']} row w-100 mx-0`}>
+					<div className="col-12 col-md-3 col-xl-3 d-flex flex-column gap-3 d-none d-md-block">
+						<div className="position-sticky" style={{ top: '50px' }}>
+							{circleLogos.map((logo, index) => (
+								<div
+									key={index}
+									className={`${styles['TIL-sideBarCircle']} mx-auto p-0`}
+								>
+									<Image
+										src={logo}
+										alt={`Logo ${index + 1}`}
+										width={70}
+										height={70}
+										className={styles['TIL-sideBarImage']}
+									/>
 								</div>
-								<div className="col-lg-9 col-md-8 m-0 text-center text-md-start">
-									<p>{shop.phone}</p>
-								</div>
-							</div>
-
-							<div className="d-flex flex-row row me-lg-5 gap-3 gap-md-0">
-								<div className="col-lg-3 col-md-4 m-0 text-center">
-									<h3>商家地址：</h3>
-								</div>
-								<div className="col-lg-9 col-md-8 m-0 text-center text-md-start">
-									<p>{shop.address}</p>
-								</div>
-							</div>
-
-							<div className="d-flex flex-row row me-lg-5 gap-3 gap-md-0">
-								<div className="col-lg-3 col-md-4 m-0 text-center">
-									<h3>關於我們：</h3>
-								</div>
-								<div className="col-lg-9 col-md-8 m-0 text-center text-md-start">
-									<p>{shop.description}</p>
-								</div>
-							</div>
-						</div>
-
-						{/* Shop Logo */}
-						<div
-							className={`${styles['TIL-detail-logo']} my-auto col-lg-4 col-md-3 col-12 mx-auto`}
-						>
-							<Image
-								src={shop.logo_path}
-								alt={shop.name}
-								width={50}
-								height={50}
-								className={`${styles['TIL-logo']}`}
-							/>
+							))}
 						</div>
 					</div>
-					<CircularSlider />
+
+					{/* Shop Details Section */}
+					<div className="col-md-9 col-xl-9 d-flex flex-column gap-3 gap-md-5 my-5">
+						<div className="d-flex flex-md-row justify-content-center flex-column-reverse px-md-3 px-lg-5">
+							<div
+								className={`${styles['TIL-shopContent']} col-lg-8 col-md-8 mx-lg-5 d-flex flex-column pe-md-2 me-md-3 col-12 p-3 p-lg-0 text-white`}
+							>
+								<h1 className="my-5 text-white">{shop.name}</h1>
+								<div className="d-flex flex-row row me-lg-5 gap-3 gap-md-0 ">
+									<div className="col-lg-3 col-md-4 m-0 text-center">
+										<h3>聯絡電話：</h3>
+									</div>
+									<div className="col-lg-9 col-md-8 m-0 text-center text-md-start">
+										<p>{shop.phone}</p>
+									</div>
+								</div>
+
+								<div className="d-flex flex-row row me-lg-5 gap-3 gap-md-0">
+									<div className="col-lg-3 col-md-4 m-0 text-center">
+										<h3>商家地址：</h3>
+									</div>
+									<div className="col-lg-9 col-md-8 m-0 text-center text-md-start">
+										<p>{shop.address}</p>
+									</div>
+								</div>
+
+								<div className="d-flex flex-row row me-lg-5 gap-3 gap-md-0">
+									<div className="col-lg-3 col-md-4 m-0 text-center">
+										<h3>關於我們：</h3>
+									</div>
+									<div className="col-lg-9 col-md-8 m-0 text-center text-md-start">
+										<p>{shop.description}</p>
+									</div>
+								</div>
+							</div>
+							<div
+								className={`${styles['TIL-detail-logo']} my-auto col-lg-4 col-md-3 col-12 mx-auto`}
+							>
+								<Image
+									src={shop.logo_path}
+									alt={shop.name}
+									width={50}
+									height={50}
+									className={`${styles['TIL-logo']}`}
+								/>
+							</div>
+						</div>
+						<CircularSlider />
+					</div>
 				</div>
+				<Footer />
 			</div>
-			<Footer />
 		</>
 	);
 }
