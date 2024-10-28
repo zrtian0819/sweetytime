b9fe80d35986e659e7769f3e5e4f11458207dc01
Jolin <lonely507487@gmail.com>
2024-10-29 01:05:12 +0800

    test[shop]嘗試使用視窗Hook切換細節頁RWD sideBar position

--- client/pages/shop/shopDetail.js
+++ client/pages/shop/shopDetail.js
@@ -4,6 +4,36 @@ import Header from '@/components/header';
 import Footer from '@/components/footer';
 import Image from 'next/image';
 import CircularSlider from '@/components/shop/shop-detail/CircularSlider';
+import Link from 'next/link';
+
+//視窗Hook
+const LAYOUT = {
+	MOBILE: 'mobile',
+	DESKTOP: 'desktop',
+};
+
+const MAX_MOBILE_WIDTH = 768;
+
+const useMedia = () => {
+	const [currentLayout, setCurrentLayout] = useState(LAYOUT.MOBILE);
+
+	useEffect(() => {
+		const handleWindowWidth = () => {
+			if (window.innerWidth < MAX_MOBILE_WIDTH) {
+				setCurrentLayout(LAYOUT.MOBILE);
+			} else {
+				setCurrentLayout(LAYOUT.DESKTOP);
+			}
+		};
+
+		handleWindowWidth();
+		window.addEventListener('resize', handleWindowWidth);
+
+		return () => window.removeEventListener('resize', handleWindowWidth);
+	}, []);
+
+	return currentLayout;
+};
 
 export default function ShopDetail() {
 	const circleLogos = [
@@ -21,10 +51,12 @@ export default function ShopDetail() {
 			'SEASON Artisan Pâtissier於2011年8月品牌誕生,在台北大直豪宅靜巷中發跡,也是甜點市場中人氣居高不墜的話題品牌,全台唯一提供多款現做盤式甜點的甜點概念餐廳,以獨特的個性和精緻的產品廣受媒體和美食同好熱愛推崇',
 		logo_path: '/photos/shop_logo/SEASON_Artisan_Pâtissier_logo.jpg',
 	};
+
 	const footerRef = useRef(null);
 	const sidebarRef = useRef(null);
 	const [isSidebarFixed, setIsSidebarFixed] = useState(true);
 	const [activeIndex, setActiveIndex] = useState(0);
+	const currentLayout = useMedia();
 
 	useEffect(() => {
 		const handleScroll = () => {
@@ -40,7 +72,6 @@ export default function ShopDetail() {
 		return () => window.removeEventListener('scroll', handleScroll);
 	}, []);
 
-	// 設定自動切換
 	const imagesToShow = 4;
 	const autoSlideInterval = 3000;
 
@@ -51,7 +82,6 @@ export default function ShopDetail() {
 		return () => clearInterval(interval);
 	}, [circleLogos.length]);
 
-	// 觸控事件處理
 	let touchStartX = 0;
 	let touchEndX = 0;
 
@@ -82,62 +112,68 @@ export default function ShopDetail() {
 					className={`${styles['TIL-shopDetailALL']} row w-100 mx-0 d-flex flex-column-reverse flex-md-row`}
 				>
 					{/* 側邊欄 */}
-					<div className="col-12 col-md-3 col-xl-3 d-flex gap-3 justify-content-center">
+					<div className="col-12 col-md-3 d-flex gap-3 justify-content-center">
 						<div
-							className="d-flex flex-row flex-md-column m-auto gap-4"
+							className={`d-flex flex-row flex-md-column m-auto gap-sm-5 gap-3`}
 							style={{
-								position: isSidebarFixed ? 'fixed' : 'relative',
-								top: isSidebarFixed ? '53px' : 'unset',
-								bottom: isSidebarFixed ? 'unset' : '20px',
+								position:
+									isSidebarFixed && currentLayout !== LAYOUT.MOBILE
+										? 'fixed'
+										: 'relative',
+								top: currentLayout === LAYOUT.MOBILE ? 'unset' : '53px',
+								bottom: currentLayout === LAYOUT.MOBILE ? '20px' : 'unset',
 							}}
 							ref={sidebarRef}
 						>
 							{visibleImages.map((logo, index) => (
-								<div
-									key={index}
-									className={`${styles['TIL-sideBarCircle']} mx-auto p-0`}
-								>
-									<Image
-										src={logo}
-										alt={`Logo ${index + 1}`}
-										width={70}
-										height={70}
-										className={styles['TIL-sideBarImage']}
-									/>
-								</div>
+								<Link href={`/shop`}>
+									<div
+										key={index}
+										className={`${styles['TIL-sideBarCircle']} mx-auto p-0`}
+									>
+										<Image
+											src={logo}
+											alt={`Logo ${index + 1}`}
+											width={70}
+											height={70}
+											className={styles['TIL-sideBarImage']}
+										/>
+									</div>
+								</Link>
 							))}
 						</div>
 					</div>
 
-					<div className="col-md-9 col-xl-9 d-flex flex-column gap-5 my-5">
-						<div className="d-flex flex-md-row justify-content-center flex-column-reverse px-md-3 px-lg-5">
+					{/* 主要內容 */}
+					<div className="col-md-9 d-flex flex-column gap-5 my-5">
+						<div className="d-flex flex-lg-row justify-content-center flex-column-reverse px-md-3 px-lg-5">
 							<div
-								className={`${styles['TIL-shopContent']} col-lg-8 col-md-8 mx-lg-5 d-flex flex-column pe-md-2 me-md-3 col-12 p-3 p-lg-0 text-white gap-4`}
+								className={`${styles['TIL-shopContent']} col-lg-8 mx-lg-5 d-flex flex-column pe-md-2 me-md-3 col-12 p-3 text-white gap-4`}
 							>
 								<h1 className="my-lg-5 text-white">{shop.name}</h1>
-								<div className="d-flex flex-row row me-lg-5 gap-1 gap-md-0 ">
-									<div className="col-lg-3 col-md-4 m-0 text-center">
+								<div className="d-flex flex-row row me-lg-5 gap-1 gap-md-0">
+									<div className="col-lg-3 col-md-4 text-center">
 										<h3>聯絡電話：</h3>
 									</div>
-									<div className="col-lg-9 col-md-8 m-0 text-center text-md-start">
+									<div className="col-lg-9 col-md-8 text-center text-md-start">
 										<p>{shop.phone}</p>
 									</div>
 								</div>
 
 								<div className="d-flex flex-row row me-lg-5 gap-1 gap-md-0">
-									<div className="col-lg-3 col-md-4 m-0 text-center ">
+									<div className="col-lg-3 col-md-4 text-center">
 										<h3>商家地址：</h3>
 									</div>
-									<div className="col-lg-9 col-md-8 m-0 text-center text-md-start">
+									<div className="col-lg-9 col-md-8 text-center text-md-start">
 										<p>{shop.address}</p>
 									</div>
 								</div>
 
 								<div className="d-flex flex-row row me-lg-5 gap-1 gap-md-0">
-									<div className="col-lg-3 col-md-4 m-0 text-center">
+									<div className="col-lg-3 col-md-4 text-center">
 										<h3>關於我們：</h3>
 									</div>
-									<div className="col-lg-9 col-md-8 m-0">
+									<div className="col-lg-9 col-md-8">
 										<p className="text-start">{shop.description}</p>
 									</div>
 								</div>
