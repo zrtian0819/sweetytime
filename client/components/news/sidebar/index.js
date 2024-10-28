dc716b0930a009616c3fc235a99d51a4106afb76
Salmon-0103 <absolute.still@gmail.com>
2024-10-28 21:17:52 +0800

    refacter(news):調整頁面卡片區及sidebar外觀,增加篩選

--- client/components/news/sidebar/index.js
+++ client/components/news/sidebar/index.js
@@ -7,14 +7,13 @@ export default function SidebarNews(props) {
 	return (
 		<>
 			<div
-				className={`${styles['LYT-sidebar']} d-flex justify-content-evenly align-items-center mb-2`}
+				className={`${styles['LYT-sidebar-news']} d-flex justify-content-evenly align-items-center mb-2`}
 			>
 				<div className={`${styles['LYT-time-box']} time-box text-center me-2`}>
 					<h4>蛋糕</h4>
 				</div>
 				<div className={`${styles['LYT-class-box']} class-box text-center`}>
-					<h4>蒙布朗蛋糕</h4>
-					<h4>NTD 1500</h4>
+					<p>美味料理食譜：經典法式甜點！檸檬萊姆塔的酸甜滋味</p>
 				</div>
 				<button className="btn">
 					<FontAwesomeIcon icon={faArrowRight} size="2x" />
