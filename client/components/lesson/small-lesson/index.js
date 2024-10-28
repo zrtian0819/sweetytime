f3fc9fa6393861ce9e445322bd5e19d752b19397
Tzu <jf76501@gmail.com>
2024-10-28 15:24:46 +0800

    refactor(lesson)課程列表頁手機版加入按鈕功能

--- client/components/lesson/small-lesson/index.js
+++ client/components/lesson/small-lesson/index.js
@@ -6,14 +6,14 @@ export default function SmLesson(props) {
 	return (
 		<>
 			<div
-				className={`${styles['CTH-sm-lesson']} d-flex justify-content-evenly align-items-center mb-2`}
+				className={`${styles['CTH-sm-lesson']} d-flex justify-content-between align-items-center`}
 			>
 				<div className={`${styles['CTH-time-box']} time-box text-center me-2`}>
 					<h4>十月</h4>
 					<h5>05</h5>
 					<h5>(日)</h5>
 				</div>
-				<div className={`${styles['CTH-class-box']} class-box text-center`}>
+				<div className="text-center">
 					<h4>蒙布朗蛋糕</h4>
 					<h4>NTD 1500</h4>
 				</div>
