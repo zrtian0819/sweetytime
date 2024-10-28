9e48d17b8abe6f16e72c108840aaec62b7a2764f
Tzu <jf76501@gmail.com>
2024-10-28 10:46:02 +0800

    feat(lesson)增加tag的active

--- client/pages/lesson/index.js
+++ client/pages/lesson/index.js
@@ -6,6 +6,8 @@ import LessonCard from '@/components/lesson/lesson-card';
 import SmLesson from '@/components/lesson/small-lesson';
 import FilterBox from '@/components/filter-box';
 import Tags from '@/components/lesson/tag';
+import { CiFilter } from 'react-icons/ci';
+import { FaRegCalendarAlt } from 'react-icons/fa';
 import Pagination from '@/components/pagination';
 import Footer from '@/components/footer';
 import styles from '@/styles/lesson.module.scss';
@@ -23,9 +25,21 @@ export default function Lesson() {
 					</div>
 					<Tags />
 				</div>
-				<div className="filter-zone-mb d-md-none d-block">
-					<FilterBox />
+				<div className="filter-box d-flex d-md-none justify-content-center gap-3">
+					<input
+						type="text"
+						className={`${styles['CTH-keywords']}`}
+						id="keywords"
+						placeholder="搜尋課程"
+					/>
+					<button className={styles['CTH-search']}>
+						<CiFilter className={styles['CTH-icon']} />
+					</button>
+					<button className={styles['CTH-calendar']}>
+						<FaRegCalendarAlt className={styles['CTH-icon']} />
+					</button>
 				</div>
+
 				<div className="lesson-info row justify-content-between">
 					<div className="lesson-card-group d-flex flex-wrap col-lg-9 col-md-8 justify-content-around">
 						<LessonCard />
@@ -45,7 +59,7 @@ export default function Lesson() {
 					</div>
 				</div>
 			</div>
-			<div className='mb-3'>
+			<div className="mb-3">
 				<Pagination
 					currentPage={1}
 					totalPages={5}
