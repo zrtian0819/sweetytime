bdd1662d71d6b663de5d6d44f1d4afa27995ce4c
Jolin <lonely507487@gmail.com>
2024-10-29 13:46:21 +0800

    feat[user]新增我的收藏(shop)

--- client/pages/user/collection/shop/index.js
+++ client/pages/user/collection/shop/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Header from '@/components/header';
 import Footer from '@/components/footer';
 import UserLeft from '@/components/user-left';
-import Shop from '@/components/shop';
+import ShopCard from '@/components/shop/shopCard';
 import Pagination from '@/components/pagination';
 import Styles from '@/styles/user.module.scss';
 
-export default function Index() {
+export default function Shop() {
+	const mockShop = { shop_id: 1, name: '花磚甜點', logo: 'sugar_logo.png', fav: false };
+
 	return (
 		<>
 			<Header />
@@ -14,13 +16,62 @@ export default function Index() {
 				<div className={`${Styles['TIL-userbody']}`}>
 					<UserLeft />
 					<div className={`${Styles['TIL-user-right']}`}>
-						<div className="d-flex flex-row flex-wrap justify-content-center gap-4">
-							<Shop />
-							<Shop />
-							<Shop />
-							<Shop />
-							<Shop />
-							<Shop />
+						<div className="d-flex flex-row flex-wrap justify-content-center h-auto gap-5">
+							<ShopCard
+								shop={mockShop}
+								onToggleFav={() => {}}
+								initStateFav={mockShop.fav}
+							/>
+							<ShopCard
+								shop={mockShop}
+								onToggleFav={() => {}}
+								initStateFav={mockShop.fav}
+							/>
+							<ShopCard
+								shop={mockShop}
+								onToggleFav={() => {}}
+								initStateFav={mockShop.fav}
+							/>
+							<ShopCard
+								shop={mockShop}
+								onToggleFav={() => {}}
+								initStateFav={mockShop.fav}
+							/>
+							<ShopCard
+								shop={mockShop}
+								onToggleFav={() => {}}
+								initStateFav={mockShop.fav}
+							/>
+							<ShopCard
+								shop={mockShop}
+								onToggleFav={() => {}}
+								initStateFav={mockShop.fav}
+							/>
+							<ShopCard
+								shop={mockShop}
+								onToggleFav={() => {}}
+								initStateFav={mockShop.fav}
+							/>
+							<ShopCard
+								shop={mockShop}
+								onToggleFav={() => {}}
+								initStateFav={mockShop.fav}
+							/>
+							<ShopCard
+								shop={mockShop}
+								onToggleFav={() => {}}
+								initStateFav={mockShop.fav}
+							/>
+							<ShopCard
+								shop={mockShop}
+								onToggleFav={() => {}}
+								initStateFav={mockShop.fav}
+							/>
+							<ShopCard
+								shop={mockShop}
+								onToggleFav={() => {}}
+								initStateFav={mockShop.fav}
+							/>
 						</div>
 						<div className="m-auto">
 							<Pagination
