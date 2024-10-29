a0468850d9403891570a0aef629407c787204e97
Peng <tmpss94319@gmail.com>
2024-10-29 12:02:46 +0800

    移動icon篩選器的圖源的路徑

--- client/components/iconClassFilter/index.js
+++ client/components/iconClassFilter/index.js
@@ -9,45 +9,49 @@ export default function IconClassFilter() {
 				<div className={`${Styles['iconContainer']} mx-sm-4`}>
 					<Image
 						className={Styles['icon']}
-						src="/iconClassFilter/filter-cupcake.svg"
-						fill
-					/>
-				</div>
-				<div className={`${Styles['iconContainer']} mx-sm-4`}>
-					<Image className={Styles['icon']} src="/iconClassFilter/filter-cake.svg" fill />
-				</div>
-				<div className={`${Styles['iconContainer']} mx-sm-4`}>
-					<Image
-						className={Styles['icon']}
-						src="/iconClassFilter/filter-cakeSlice.svg"
+						src="/icon/iconClassFilter/filter-cupcake.svg"
 						fill
 					/>
 				</div>
 				<div className={`${Styles['iconContainer']} mx-sm-4`}>
 					<Image
 						className={Styles['icon']}
-						src="/iconClassFilter/filter-donuts.svg"
+						src="/icon/iconClassFilter/filter-cake.svg"
 						fill
 					/>
 				</div>
 				<div className={`${Styles['iconContainer']} mx-sm-4`}>
 					<Image
 						className={Styles['icon']}
-						src="/iconClassFilter/filter-pineappleBread.svg"
+						src="/icon/iconClassFilter/filter-cakeSlice.svg"
 						fill
 					/>
 				</div>
 				<div className={`${Styles['iconContainer']} mx-sm-4`}>
 					<Image
 						className={Styles['icon']}
-						src="/iconClassFilter/filter-croissant.svg"
+						src="/icon/iconClassFilter/filter-donuts.svg"
 						fill
 					/>
 				</div>
 				<div className={`${Styles['iconContainer']} mx-sm-4`}>
 					<Image
 						className={Styles['icon']}
-						src="/iconClassFilter/filter-cinnamonRoll.svg"
+						src="/icon/iconClassFilter/filter-pineappleBread.svg"
+						fill
+					/>
+				</div>
+				<div className={`${Styles['iconContainer']} mx-sm-4`}>
+					<Image
+						className={Styles['icon']}
+						src="/icon/iconClassFilter/filter-croissant.svg"
+						fill
+					/>
+				</div>
+				<div className={`${Styles['iconContainer']} mx-sm-4`}>
+					<Image
+						className={Styles['icon']}
+						src="/icon/iconClassFilter/filter-cinnamonRoll.svg"
 						fill
 					/>
 				</div>
