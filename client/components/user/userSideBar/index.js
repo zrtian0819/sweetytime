953e9898d6689af827dcec8d3f1a49bd4ebc11b1
Jolin <lonely507487@gmail.com>
2024-10-31 02:38:20 +0800

    test、fix[user]重整讓sidebar元件程式碼變簡潔好管理

--- client/components/user/userSideBar/index.js
+++ client/components/user/userSideBar/index.js
@@ -8,163 +8,97 @@ import ListItemText from '@mui/material/ListItemText';
 import Collapse from '@mui/material/Collapse';
 import { FaPen } from 'react-icons/fa';
 
-export default function UserSideBar(props) {
-	const [open, setOpen] = useState([false, false, false]);
+export default function UserSideBar() {
+	const [open, setOpen] = useState([false, false, false, false]);
 
 	const handleClick = (index) => {
 		const newOpen = open.map((isOpen, i) => (i === index ? !isOpen : isOpen));
 		setOpen(newOpen);
 	};
+
+	const menuItems = [
+		{
+			id: 0,
+			icon: '/icon/account.svg',
+			label: '我的帳戶',
+			subItems: ['個人檔案', '地址管理'],
+		},
+		{
+			id: 1,
+			icon: '/icon/box-outline.svg',
+			label: '我的收藏',
+			subItems: ['收藏商品', '收藏店家', '收藏課程'],
+		},
+		{
+			id: 2,
+			icon: '/icon/symbols_list.svg',
+			label: '購買清單',
+			subItems: ['歷史訂單', '我的課程'],
+		},
+		{
+			id: 3,
+			icon: '/icon/coupon-line.svg',
+			label: '會員好康',
+			subItems: [],
+		},
+	];
+
 	return (
-		<>
-			<div className={`${Styles['TIL-user-left']} mx-auto d-flex flex-column`}>
-				<div className={`${Styles['TIL-bg']} d-flex flex-column gap-4 py-5`}>
-					<div className={`${Styles['TIL-user-image']} mx-auto`}>
-						<Img
-							src="/photos/users/user_panda.png"
-							alt="頭像"
-							width={100}
-							height={100}
-							className="rounded-circle object-fit-cover shadow-sm w-100"
-						/>
-					</div>
-					<div
-						className={`${Styles['CTH-header-right']} d-block d-md-none text-center ms-2`}
-					>
-						<h1 className="mb-3">AiBao_baoFamily0524</h1>
-						<button className={`${Styles['CTH-btn']} d-flex align-items-center m-auto`}>
-							<div className="me-2">管理個人資料</div>
-							<FaPen />
-						</button>
-					</div>
+		<div className={`${Styles['TIL-user-left']} mx-auto d-flex flex-column`}>
+			<div className={`${Styles['TIL-bg']} d-flex flex-column gap-4 py-5`}>
+				<div className={`${Styles['TIL-user-image']} mx-auto`}>
+					<Img
+						src="/photos/users/user_panda.png"
+						alt="頭像"
+						width={100}
+						height={100}
+						className="rounded-circle object-fit-cover shadow-sm w-100"
+					/>
 				</div>
-				<div
-					className={`${Styles['TIL-navBar']} d-flex container justify-content-center align-items-start align-items-md-center flex-row flex-md-column w-100 px-2 m-0`}
-				>
-					<List>
-						<ListItemButton
-							className={`${Styles['TIL-nav']}`}
-							onClick={() => {
-								handleClick(0);
-							}}
-						>
-							<Img
-								src={'/icon/account.svg'}
-								width={25}
-								height={25}
-								className="me-1"
-							/>
-							<ListItemText primary="我的帳戶" />
-							{open[0] ? '' : ''}
-						</ListItemButton>
-						<Collapse
-							in={open[0]}
-							timeout="auto"
-							unmountOnExit
-							className={Styles['TIL-nav']}
-						>
-							<List
-								component="div"
-								disablePadding
-								className={Styles['TIL-items-box']}
-							>
-								<ListItemButton sx={{ pl: 4 }} className={Styles['TIL-items']}>
-									<ListItemText primary="個人檔案" />
-								</ListItemButton>
-								<ListItemButton sx={{ pl: 4 }} className={Styles['TIL-items']}>
-									<ListItemText primary="地址管理" />
-								</ListItemButton>
-							</List>
-						</Collapse>
-					</List>
-					<List>
-						<ListItemButton
-							className={`${Styles['TIL-nav']}`}
-							onClick={() => {
-								handleClick(1);
-							}}
-						>
-							<Img
-								src={'/icon/box-outline.svg'}
-								width={25}
-								height={25}
-								className="me-2"
-							/>
-							<ListItemText primary="我的收藏" />
-							{open[1] ? '' : ''}
-						</ListItemButton>
-						<Collapse
-							in={open[1]}
-							timeout="auto"
-							unmountOnExit
-							className={Styles['TIL-nav']}
-						>
-							<List
-								component="div"
-								disablePadding
-								className={Styles['TIL-items-box']}
-							>
-								<ListItemButton sx={{ pl: 4 }} className={Styles['TIL-items']}>
-									<ListItemText primary="收藏商品" />
-								</ListItemButton>
-								<ListItemButton sx={{ pl: 4 }} className={Styles['TIL-items']}>
-									<ListItemText primary="收藏店家" />
-								</ListItemButton>
-								<ListItemButton sx={{ pl: 4 }} className={Styles['TIL-items']}>
-									<ListItemText primary="收藏課程" />
-								</ListItemButton>
-							</List>
-						</Collapse>
-					</List>
-					<List>
-						<ListItemButton
-							className={`${Styles['TIL-nav']}`}
-							onClick={() => {
-								handleClick(2);
-							}}
-						>
-							<Img
-								src={'/icon/symbols_list.svg'}
-								width={25}
-								height={25}
-								className="me-2"
-							/>
-							<ListItemText primary="購買清單" />
-							{open[2] ? '' : ''}
-						</ListItemButton>
-						<Collapse
-							in={open[2]}
-							timeout="auto"
-							unmountOnExit
-							className={Styles['TIL-nav']}
-						>
-							<List
-								component="div"
-								disablePadding
-								className={Styles['TIL-items-box']}
-							>
-								<ListItemButton sx={{ pl: 4 }} className={Styles['TIL-items']}>
-									<ListItemText primary="歷史訂單" />
-								</ListItemButton>
-								<ListItemButton sx={{ pl: 4 }} className={Styles['TIL-items']}>
-									<ListItemText primary="我的課程" />
-								</ListItemButton>
-							</List>
-						</Collapse>
-					</List>
-					<List>
-						<ListItemButton onClick={handleClick} className={Styles['TIL-nav']}>
-							<Img
-								src={'/icon/coupon-line.svg'}
-								width={25}
-								height={25}
-								className="me-2"
-							/>
-							<ListItemText primary="會員好康" />
-						</ListItemButton>
-					</List>
+				<div className={`${Styles['CTH-header-right']} d-block d-md-none text-center ms-2`}>
+					<h1 className="mb-3">AiBao_baoFamily0524</h1>
+					<button className={`${Styles['CTH-btn']} d-flex align-items-center m-auto`}>
+						<div className="me-2">管理個人資料</div>
+						<FaPen />
+					</button>
 				</div>
 			</div>
-		</>
+
+			{/* 導航欄 */}
+			<div
+				className={`${Styles['TIL-navBar']} d-flex flex-row gx-2 gy-2 p-2 flex-md-column m-0`}
+			>
+				{menuItems.map((item) => (
+					<div key={item.id} className="">
+						<List className="d-flex flex-column align-items-center">
+							<ListItemButton
+								className={`${Styles['TIL-nav']} `}
+								onClick={() => handleClick(item.id)}
+							>
+								<Img src={item.icon} width={25} height={25} className="me-2" />
+								<ListItemText primary={item.label} />
+							</ListItemButton>
+							<Collapse in={open[item.id]} timeout="auto" unmountOnExit>
+								<List
+									component="div"
+									disablePadding
+									className={`${Styles['TIL-items-box']} justify-content-end`}
+								>
+									{item.subItems.map((subItem) => (
+										<ListItemButton
+											key={subItem}
+											sx={{ pl: 4, justifyContent: 'flex-end' }}
+											className={Styles['TIL-items']}
+										>
+											<ListItemText primary={subItem} />
+										</ListItemButton>
+									))}
+								</List>
+							</Collapse>
+						</List>
+					</div>
+				))}
+			</div>
+		</div>
 	);
 }
