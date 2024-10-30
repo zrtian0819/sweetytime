636082bbaf1bdfa730c5749c85224bd4166c7fae
Jolin <lonely507487@gmail.com>
2024-10-30 16:15:06 +0800

    fix[user]會員中心外框元件完成

--- client/components/user/userBox/index.js
+++ client/components/user/userBox/index.js
@@ -5,14 +5,19 @@ import Styles from '@/components//user/userBox/userBox.module.scss';
 export default function UserBox({ children }) {
 	return (
 		<>
-			<div className={`${Styles['TIL-body']} test-mode`}>
+			<div className={`${Styles['TIL-body']}`}>
 				<div className={`${Styles['TIL-userbody']} container `}>
-					<div className="row h-100">
+					<div className={`${Styles['TIL-user-box']} row`}>
 						<div className="col-12 col-md-2 pe-md-2 p-0 h-md-00 ">
 							<UserSidebar />
 						</div>
-						<div className={`${Styles['TIL-user-right']} col-10 col-md-10 h-lg-100`}>
-							{children}
+						<div
+							className={`${Styles['TIL-user-right']} col-10 col-md-10 h-lg-100 p-0 p-md-5`}
+						>
+							<div className={`${Styles['TIL-content-title']} d-block d-md-none`}>
+								修改個人資料
+							</div>
+							<div>{children}</div>
 						</div>
 					</div>
 				</div>
--- client/pages/user/collection/lesson/index.js
+++ client/pages/user/collection/lesson/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Header from '@/components/header';
 import Footer from '@/components/footer';
-// import Styles from '@/styles/user.module.scss';
 import UserBox from '@/components/user/userBox';
 import LessonCard from '@/components/lesson/lesson-card';
 import Pagination from '@/components/pagination';
@@ -11,16 +10,24 @@ export default function Lesson() {
 		<>
 			<Header />
 			<UserBox>
-				<div className="d-flex flex-row flex-wrap justify-content-center">
-					<LessonCard />
-				</div>
-				<div className="m-auto">
-					<Pagination
-						currentPage={1}
-						totalPages={5}
-						onPageChange={() => {}}
-						changeColor="#fe6f67"
-					/>
+				{/* 此頁面再自己修整 */}
+				<div className="d-flex flex-column py-5 p-md-0 gap-3">
+					<div className="d-flex flex-row flex-wrap justify-content-center">
+						<LessonCard />
+						<LessonCard />
+						<LessonCard />
+						<LessonCard />
+						<LessonCard />
+						<LessonCard />
+					</div>
+					<div className="m-auto">
+						<Pagination
+							currentPage={1}
+							totalPages={5}
+							onPageChange={() => {}}
+							changeColor="#fe6f67"
+						/>
+					</div>
 				</div>
 			</UserBox>
 			<Footer bgColor="#fcf3ea" />
