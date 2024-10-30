60755a4be8c59909c6ec7d4b551f02361b7230a9
Jolin <lonely507487@gmail.com>
2024-10-30 17:25:56 +0800

    refactor[user]我的收藏套用新元件(product、shop)

--- client/pages/user/collection/product/index.js
+++ client/pages/user/collection/product/index.js
@@ -1,39 +1,35 @@
 import React from 'react';
 import Header from '@/components/header';
 import Footer from '@/components/footer';
-import UserLeft from '@/components/user-left';
+import UserBox from '@/components/user/userBox';
 import ProductCard from '@/components/product-card';
 import Pagination from '@/components/pagination';
-import Styles from '@/styles/user.module.scss';
 
-export default function Product() {
+export default function Lesson() {
 	return (
 		<>
 			<Header />
-			<div className={`${Styles['TIL-body']} mt-5 d-none d-md-flex flex-column container`}>
-				<div className={`${Styles['TIL-userbody']}`}>
-					<UserLeft />
-					<div className={`${Styles['TIL-user-right']}`}>
-						<div className="d-flex flex-row flex-wrap justify-content-center gap-4">
-							<ProductCard />
-							<ProductCard />
-							<ProductCard />
-							<ProductCard />
-							<ProductCard />
-							<ProductCard />
-						</div>
-						<div className="m-auto">
-							<Pagination
-								currentPage={1}
-								totalPages={5}
-								onPageChange={() => {}}
-								changeColor="#fe6f67"
-							/>
-						</div>
+			<UserBox>
+				<div className="d-flex flex-column py-5 p-md-0 gap-3">
+					<div className="d-flex flex-row flex-wrap justify-content-center gap-5">
+						<ProductCard />
+						<ProductCard />
+						<ProductCard />
+						<ProductCard />
+						<ProductCard />
+						<ProductCard />
+					</div>
+					<div className="m-auto">
+						<Pagination
+							currentPage={1}
+							totalPages={5}
+							onPageChange={() => {}}
+							changeColor="#fe6f67"
+						/>
 					</div>
 				</div>
-			</div>
-			<Footer />
+			</UserBox>
+			<Footer bgColor="#fcf3ea" />
 		</>
 	);
 }
