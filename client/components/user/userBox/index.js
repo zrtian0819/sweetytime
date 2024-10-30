9a7d1d3b5f0f0407d667b23a7634c161ac0c192b
Jolin <lonely507487@gmail.com>
2024-10-30 22:06:02 +0800

    fix[user]修改程式碼，消除捲軸、refactor[]user修整SCSS

--- client/components/user/userBox/index.js
+++ client/components/user/userBox/index.js
@@ -1,27 +1,23 @@
 import React from 'react';
 import UserSidebar from '@/components/user/userSideBar';
-import Styles from '@/components//user/userBox/userBox.module.scss';
+import Styles from '@/components/user/userBox/userBox.module.scss';
 
 export default function UserBox({ children }) {
 	return (
-		<>
-			<div className={`${Styles['TIL-body']}`}>
-				<div className={`${Styles['TIL-userbody']} container `}>
-					<div className={`${Styles['TIL-user-box']} row`}>
-						<div className="col-12 col-md-2 pe-md-2 p-0 h-md-00 ">
-							<UserSidebar />
+		<div className={`${Styles['TIL-body']}`}>
+			<div className={`${Styles['TIL-userbody']} container`}>
+				<div className={`${Styles['TIL-user-box']} row`}>
+					<div className="col-12 col-md-2 pe-md-2 p-0">
+						<UserSidebar />
+					</div>
+					<div className={`${Styles['TIL-user-right']} col-12 col-md-10 p-0 p-md-5`}>
+						<div className={`${Styles['TIL-content-title']} d-block d-md-none`}>
+							修改個人資料
 						</div>
-						<div
-							className={`${Styles['TIL-user-right']} col-10 col-md-10 h-lg-100 p-0 p-md-5`}
-						>
-							<div className={`${Styles['TIL-content-title']} d-block d-md-none`}>
-								修改個人資料
-							</div>
-							<div>{children}</div>
-						</div>
+						<div>{children}</div>
 					</div>
 				</div>
 			</div>
-		</>
+		</div>
 	);
 }
