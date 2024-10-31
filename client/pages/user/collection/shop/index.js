36fc8deb6af81d8a4414ec561ab862a57e3f877a
Jolin <lonely507487@gmail.com>
2024-10-31 18:32:22 +0800

    feat[user]新增我的收藏搜尋UI

--- client/pages/user/collection/shop/index.js
+++ client/pages/user/collection/shop/index.js
@@ -4,6 +4,8 @@ import Footer from '@/components/footer';
 import UserBox from '@/components/user/userBox';
 import ShopCard from '@/components/shop/shopCard';
 import Pagination from '@/components/pagination';
+import Styles from '@/styles/user.module.scss';
+import { FaSearch } from 'react-icons/fa';
 
 export default function Shop() {
 	const mockShop = { shop_id: 1, name: '花磚甜點', logo: 'sugar_logo.png', fav: false };
@@ -13,6 +15,12 @@ export default function Shop() {
 			<Header />
 			<UserBox>
 				<div className="d-flex flex-column py-5 p-md-0 gap-3">
+					<div className={`${Styles['TIL-search']} d-flex justify-content-center gap-2`}>
+						<input type="text" className="px-3" placeholder="透過店家名稱搜尋" />
+						<button className={Styles['TIL-Btn']}>
+							<FaSearch size={25} className={Styles['TIL-Fa']} />
+						</button>
+					</div>
 					<div className="d-flex flex-row flex-wrap justify-content-center gap-5">
 						<ShopCard
 							shop={mockShop}
