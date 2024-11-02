2dab20b153c30e71e2a4df8b946ad3e7a8b4bea2
TzuuChen <jf76501@gmail.com>
2024-11-02 15:16:06 +0800

    feat(lesson)測試連接資料庫成功

--- client/components/lesson/lesson-card/index.js
+++ client/components/lesson/lesson-card/index.js
@@ -4,7 +4,11 @@ import Image from 'next/image';
 import { FaHeart, FaRegHeart } from 'react-icons/fa';
 import { FaArrowRightLong } from 'react-icons/fa6';
 
-export default function LessonCard(props) {
+export default function LessonCard({
+	name = '栗子蒙布朗',
+	date = '2024/11/3(日)',
+	price = 'NTD1500',
+}) {
 	const [isLike, setIsLike] = useState(false);
 	const handleLike = () => {
 		setIsLike(!isLike);
@@ -35,9 +39,9 @@ export default function LessonCard(props) {
 				</div>
 				<div className={`${Styles['CTH-card-body']} card-body`}>
 					<div className="card-body-left">
-						<h4>蒙布朗栗子蛋糕</h4>
-						<p>2024/10/5(日)</p>
-						<h4>NTD 1500</h4>
+						<h4>{name}</h4>
+						<p>{date}</p>
+						<h4>{price}</h4>
 					</div>
 					<div className={`${Styles['CTH-card-body-right']} ms-auto`}>
 						<button className="btn">
