9b7a0285a9d02ef22ef9389741beaf215be360aa
Peng <tmpss94319@gmail.com>
2024-11-04 17:48:28 +0800

    feat(product)成功取得隨機商品資料

---
+++ server/routes/product.js
@@ -0,0 +1,24 @@
+import express from 'express'
+import db from '#configs/mysql.js'
+const router = express.Router()
+
+router.get('/', async (req, res) => {
+  try {
+    const [rows] = await db.query(`
+SELECT p.*, pp.file_name 
+FROM (
+  SELECT * FROM product ORDER BY RAND() LIMIT 12
+) p
+LEFT JOIN (
+  SELECT product_id, file_name 
+  FROM product_photo 
+  ORDER BY id ASC
+) pp ON p.id = pp.product_id
+GROUP BY p.id
+`)
+    res.json(rows)
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to fetch users' })
+  }
+})
+export default router
