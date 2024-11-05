9fa9333319d583414c57cd3f94102dc9064ba585
SU0119SU <suyamaomao@gmail.com>
2024-11-05 10:02:16 +0800

    feat(teacher)新增師資列表跟細節頁路由/列表頁和細節頁及卡片串連

--- client/components/TeacherCard/index.js
+++ client/components/TeacherCard/index.js
@@ -3,13 +3,12 @@ import Link from 'next/link';
 import styles from './teacher-card.module.scss';
 
 const TeacherCard = ({ teacher }) => (
-	// <Link href={`/teacher/teacherDetail?id=${teacher.id}`} passHref>
-	<Link href={`/teacher/teacherDetail`} passHref>
+	<Link href={`/teacher/teacherDetail?id=${teacher.id}`} passHref>
 		<div className={styles.card}>
 			<h3 className={styles.teacherTitle}>{teacher.title}</h3>
 			<h3 className={styles.teacherName}>{teacher.name}</h3>
 			<div className={styles.imgBox}>
-				<img src={teacher.imgSrc} alt={teacher.name} className={styles.teacherImage} />
+				<img src={`/photos/teachers/${teacher.img_path}`} alt={teacher.name} className={styles.teacherImage} />
 			</div>
 		</div>
 	</Link>
