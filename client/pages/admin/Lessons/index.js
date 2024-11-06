f80e1a27c96b3acf6ffa3c9b1cb82219bec145a5
Tzu <jf76501@gmail.com>
2024-11-06 11:15:04 +0800

    refactor(admin)lessonList 倒資料 & 分頁完成

--- client/pages/admin/Lessons/index.js
+++ client/pages/admin/Lessons/index.js
@@ -6,13 +6,33 @@ import ViewButton from '@/components/adminCRUD/viewButton';
 import EditButton from '@/components/adminCRUD/editButton';
 import ToggleButton from '@/components/adminCRUD/toggleButton';
 import styles from '@/styles/adminLesson.module.scss';
+import axios from 'axios';
 
 export default function Lessons(props) {
 	const [isToggled, setIsToggled] = useState(false);
+	const [lesson, setLesson] = useState([]);
+	const [currentPage, setCurrentPage] = useState(1);
+	const ITEMS_PER_PAGE = 10; // 每頁顯示的卡片數量
+
+	// 計算當前頁顯示的卡片範圍
+	const startIndex = (currentPage - 1) * ITEMS_PER_PAGE;
+	const endIndex = startIndex + ITEMS_PER_PAGE;
+	const lessonToshow = lesson.slice(startIndex, endIndex);
+
+	// 計算總頁數
+	const totalPages = Math.ceil(lesson.length / ITEMS_PER_PAGE);
+
 	const handleToggleClick = () => {
 		setIsToggled(!isToggled);
 		console.log('Toggle狀態:', isToggled ? '關閉' : '開啟');
 	};
+
+	useEffect(() => {
+		axios
+			.get('http://localhost:3005/api/lesson')
+			.then((res) => setLesson(res.data))
+			.catch((error) => console.error('拿不到資料', error));
+	}, []);
 	return (
 		<>
 			<AdminLayout>
@@ -31,37 +51,51 @@ export default function Lessons(props) {
 						</tr>
 					</thead>
 					<tbody>
-						<tr class="text-center m-auto align-middle">
-							<td>1</td>
-							<td>上架中</td>
-							<td>蒙布朗栗子巧克力蛋糕</td>
-							<td>蛋糕</td>
-							<td>劉偉苓 Willin</td>
-							<td>2024-08-22 09:00</td>
-							<td>40</td>
-							<td>1</td>
-							<td>
-								<div className="d-flex gap-3 justify-content-center">
-									<Link href={'./Lessons/viewLesson'}>
-										<ViewButton />
-									</Link>
-									<Link href={'./Lessons/editLesson'}>
-										<EditButton />
-									</Link>
-									<ToggleButton
-										onClick={handleToggleClick}
-										isActive={isToggled}
-									/>
-								</div>
-							</td>
-						</tr>
+						{lesson.length > 0 ? (
+							<>
+								{lessonToshow.map((data) => {
+									return (
+										<>
+											<tr class="text-center m-auto align-middle">
+												<td>{data.id}</td>
+												<td>
+													{data.activation == 1 ? '上架中' : '已下架'}
+												</td>
+												<td>{data.name}</td>
+												<td>{data.product_class_id}</td>
+												<td>{data.teacher_id}</td>
+												<td>{data.start_date}</td>
+												<td>{data.quota}</td>
+												<td>1</td>
+												<td>
+													<div className="d-flex gap-3 justify-content-center">
+														<Link href={'./Lessons/viewLesson'}>
+															<ViewButton />
+														</Link>
+														<Link href={'./Lessons/editLesson'}>
+															<EditButton />
+														</Link>
+														<ToggleButton
+															onClick={handleToggleClick}
+															isActive={isToggled}
+														/>
+													</div>
+												</td>
+											</tr>
+										</>
+									);
+								})}
+							</>
+						) : (
+							''
+						)}
 					</tbody>
 				</table>
 				<Pagination
-					currentPage={1}
-					totalPages={5}
-					onPageChange={() => {}}
-					changeColor="#fff"
+					currentPage={currentPage}
+					totalPages={totalPages}
+					onPageChange={(page) => setCurrentPage(page)}
+					changeColor="#fe6f67"
 				/>
 			</AdminLayout>
 		</>
