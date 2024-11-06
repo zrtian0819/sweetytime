9bf13ad7799d8d8bf859c2a69a1f839631d3648e
Tzu <jf76501@gmail.com>
2024-11-06 18:06:56 +0800

    refactor(admin)課程路由新增後台

--- server/routes/lesson.js
+++ server/routes/lesson.js
@@ -11,6 +11,39 @@ router.get('/', async (req, res) => {
   }
 })
 
+router.get('/admin', async (req, res) => {
+  try {
+    const [rows] = await db.query(
+      ` SELECT
+     lesson.id,
+     lesson.name AS lesson_name,
+     lesson.start_date,
+     lesson.quota,
+     lesson.activation,
+     teacher.name AS teacher_name,
+     product_class.class_name AS class_name
+   FROM lesson 
+   JOIN product_class ON lesson.product_class_id = product_class.id 
+   JOIN teacher ON lesson.teacher_id = teacher.id
+      `
+    )
+    res.json(rows)
+  } catch (error) {
+    res.status(500).json({ error: '拿不到關聯的資料' })
+  }
+})
+
+router.get('/student', async (req, res) => {
+  try {
+    const [stu] =
+      await db.query(`SELECT lesson_id, COUNT(user_id) AS student_count
+       FROM student
+       GROUP BY lesson_id`)
+    res.json(stu)
+  } catch (error) {
+    res.status(500).json({ error: '拿不到學生資料' })
+  }
+})
 router.get('/:id', async (req, res) => {
   const { id } = req.params
   try {
@@ -22,10 +55,19 @@ router.get('/:id', async (req, res) => {
     const [teacher_rows] = await db.query(`SELECT * FROM teacher WHERE id=?`, [
       rows[0].teacher_id,
     ])
+    const [type] = await db.query(`SELECT * FROM product_class WHERE id=?`, [
+      rows[0].product_class_id,
+    ])
+    const [stu_rows] = await db.query(
+      `SELECT * FROM student WHERE lesson_id=?`,
+      [id]
+    )
     res.json({
       lesson: rows,
       photo: photo_rows,
       teacher: teacher_rows,
+      type: type,
+      stu: stu_rows,
     })
   } catch (error) {
     res.status(500).json({ error: '拿不到細節資料' })
