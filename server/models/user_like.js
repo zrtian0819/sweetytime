8086f0a19b15155e628b2a3cc566ab3a2e91bdfd
Dennis <sfnandy65066506@gmail.com>
2024-11-07 21:01:03 +0800

    feat(user_like):修改user_like資料庫設定/新增user_like資料庫內容

--- server/models/user_like.js
+++ server/models/user_like.js
@@ -9,26 +9,25 @@ export default async function (sequelize) {
         type: DataTypes.INTEGER.UNSIGNED,
         allowNull: false,
         primaryKey: true,
+        comment: '唯一識別碼'
       },
       user_id: {
         type: DataTypes.INTEGER,
         allowNull: true,
-      },
-      type_id: {
-        type: DataTypes.INTEGER,
-        allowNull: true,
+        comment: '使用者ID，關聯到users表的id'
       },
       type: {
-        type: DataTypes.STRING(225),
+        type: DataTypes.ENUM('lesson', 'shop', 'product'),
         allowNull: true,
+        comment: '收藏項目類型：課程(lesson)、商店(shop)、商品(product)'
       },
     },
     {
       sequelize,
       tableName: 'user_like',
       timestamps: true,
-      charset: 'utf8mb4', // 全域性設定 charset
-      collate: 'utf8mb4_unicode_ci', // 全域性設定 collate
+      charset: 'utf8mb4',
+      collate: 'utf8mb4_unicode_ci',
       indexes: [
         {
           name: 'PRIMARY',
