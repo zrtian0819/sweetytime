ea0e7f6c99b1edd92b6c4826fb5753d9a9167f56
Peng <tmpss94319@gmail.com>
2024-11-08 16:13:06 +0800

    feat(product)收藏按鈕成功依資料庫狀態顯示

--- server/routes/userLikedProducts.js
+++ server/routes/userLikedProducts.js
@@ -3,15 +3,14 @@ import db from '#configs/mysql.js'
 const router = express.Router()
 
 router.get('/', async (req, res) => {
+  const id = parseInt(req.query.userId)
   try {
-    const [rows] = await db.query(`
-SELECT p.*, 
-  (SELECT file_name 
-    FROM product_photo 
-    WHERE product_photo.product_id = p.id 
-    ORDER BY id ASC LIMIT 1) AS file_name
-FROM product p
-`)
+    const [rows] = await db.query(
+      `
+SELECT * FROM user_like WHERE user_id = ? AND type = ?
+`,
+      [id, 'product']
+    )
     res.json(rows)
   } catch (error) {
     res.status(500).json({ error: 'Failed to fetch product' })
