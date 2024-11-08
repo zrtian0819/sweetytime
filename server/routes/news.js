9d9d3548b7d727902cfa51920b6148ad8572d23f
Salmon-0103 <absolute.still@gmail.com>
2024-11-09 00:45:41 +0800

    refactor(news):修正路由錯誤

--- server/routes/news.js
+++ server/routes/news.js
@@ -2,8 +2,42 @@ import express from 'express'
 import db from '#configs/mysql.js'
 const router = express.Router()
 
-// 抓取所有文章
 router.get('/', async (req, res) => {
+  try {
+    const [rows] = await db.query('SELECT * FROM news')
+    res.json(rows)
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to fetch news' })
+  }
+})
+
+// 更新新聞的上下架狀態
+router.post('/admin/:newsId', async (req, res) => {
+  const { newsId } = req.params // 從路由參數取得 id
+  try {
+    const [row] = await db.query(
+      `SELECT news.activation FROM news WHERE id=?`,
+      [newsId]
+    )
+
+    if (row.length === 0) {
+      return res.status(404).json({ error: '找不到文章' })
+    }
+
+    const status = row[0].activation == 1 ? '0' : '1'
+    await db.query(`UPDATE news SET activation = ? WHERE id = ?`, [
+      status,
+      newsId,
+    ])
+
+    res.json({ activation: status })
+  } catch (error) {
+    console.error('更新失敗', error)
+    res.status(500).json({ error: '更新失敗' })
+  }
+})
+
+router.get('/admin', async (req, res) => {
   try {
     const [rows] = await db.query(
       `SELECT 
@@ -21,7 +55,7 @@ router.get('/', async (req, res) => {
     res.json(rows)
   } catch (error) {
     console.error('Error fetching news:', error)
-    res.status(500).json({ error: 'Failed to fetch news' })
+    res.status(500).json({ error: '找不到文章資料' })
   }
 })
 
@@ -29,7 +63,6 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   const { id } = req.params
   try {
-    // 抓取指定 ID 的新聞資料
     const [newsRows] = await db.query(
       `SELECT 
         news.id, 
@@ -50,7 +83,6 @@ router.get('/:id', async (req, res) => {
       return res.status(404).json({ error: '找不到文章' })
     }
 
-    // 返回單一新聞資料
     res.json(newsRows[0])
   } catch (error) {
     console.error('找不到文章資料', error)
