1b1f0a1d6318891da6a7711f24107d2befee6da5
Joe <cjt3399@gmail.com>
2024-11-11 15:14:22 +0800

    feat(homePage):首頁建立取得所有產品資訊的路由規則

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,19 @@
 import express from 'express'
+import db from '#configs/mysql.js'
 const router = express.Router()
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' })
+})
+
+router.get('/product', async (req, res) => {
+  try {
+    const [rows] = await db.query('SELECT * FROM product')
+    res.json(rows)
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to fetch product' })
+  }
 })
 
 export default router
