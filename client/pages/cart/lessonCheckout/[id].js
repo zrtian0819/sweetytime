6e0a135bd7cda63bba11f310e7161d16daaee9df
Tzu <jf76501@gmail.com>
2024-11-12 10:45:34 +0800

    feat(checkout)lesson 新增綠界付款完成

--- client/pages/cart/lessonCheckout/[id].js
+++ client/pages/cart/lessonCheckout/[id].js
@@ -8,12 +8,13 @@ import CheckoutItem from '@/components/cart/checkout-item';
 import { Button } from 'react-bootstrap';
 import { useRouter } from 'next/router';
 import axios from 'axios';
-import product from '@/pages/user/collection/product';
+import { useUser } from '@/context/userContext';
 
 export default function LessonCheckout(props) {
 	const router = useRouter();
 	const { id } = router.query;
 	const [lesson, setLesson] = useState([]);
+	const { user } = useUser();
 
 	const [payway, setPayWay] = useState('');
 
@@ -50,7 +51,23 @@ export default function LessonCheckout(props) {
 					});
 				break;
 			case 'ecpay':
-				console.log('綠界科技');
+				axios
+					.post(`http://localhost:3005/api/cart/create-order-lesson`, ecData)
+					.then((res) => {
+						try {
+							const url = new URL('http://localhost:3005/api/ecpay-test-only');
+							url.searchParams.append('amount', data.price);
+							window.location.href = url.toString();
+						} catch (error) {
+							console.error('綠界支付導向失敗:', error);
+							toast.error('支付導向失敗，請稍後再試');
+							throw new Error('綠界支付導向失敗');
+						}
+					})
+					.catch((error) => {
+						console.error('失敗', error);
+					});
+
 				break;
 		}
 	};
@@ -69,6 +86,13 @@ export default function LessonCheckout(props) {
 						time: getCurrentTime(),
 					},
 				],
+		  }
+		: {};
+	const ecData = data
+		? {
+				user_id: user.id,
+				lesson_id: data.id,
+				sign_up: getCurrentTime(),
 		  }
 		: {};
 
