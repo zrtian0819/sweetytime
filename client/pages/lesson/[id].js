ac3f219e1af641e7a606ad1667b6729311aee3ad
Tzu <jf76501@gmail.com>
2024-11-12 09:15:19 +0800

    refactor(lesson)新增名額排序

--- client/pages/lesson/[id].js
+++ client/pages/lesson/[id].js
@@ -331,7 +331,7 @@ export default function LessonDetail(props) {
 														className={styles['image']}
 													/>
 											  ))
-											: '載入中'}
+											: '還未有其他照片！'}
 									</div>
 									<div className="class-content col-12 col-md-6 d-block d-md-none">
 										<h2>課程介紹</h2>
