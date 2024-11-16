12ae0d6c8bcfbf92592ca5aef474cdb2ef57587d
Jolin <lonely507487@gmail.com>
2024-11-16 16:18:41 +0800

    refactor[後台header]修改顯示的icon&導頁&文字大小

--- client/components/adminNavbar/index.js
+++ client/components/adminNavbar/index.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 import { Navbar } from 'react-bootstrap';
-import { AiOutlineBell, AiOutlineUser } from 'react-icons/ai';
+import { IoMdHome } from 'react-icons/io';
 import { FiLogOut } from 'react-icons/fi';
 import styles from './admin-navbar.module.scss';
 import { useRouter } from 'next/router';
@@ -10,6 +10,10 @@ import { useUser } from '@/context/userContext';
 const AdminNavbar = () => {
 	const { user, logout } = useUser();
 	const router = useRouter();
+
+	const handldHome = () => {
+		router.push('/admin');
+	};
 
 	// 處理登出
 	const handleLogout = async () => {
@@ -29,26 +33,19 @@ const AdminNavbar = () => {
 					/>
 				</Link>
 			</div>
-			<div className="d-flex align-items-center ml-auto">
+			<div className="d-flex align-items-center ml-auto gap-2">
 				<div className={styles.Badge}>Hello, {user?.name || ''}</div>
+				<div className={styles.role}>{user?.role || ''}</div>
 
 				{/* Icon Group */}
 				<div className={`${styles.iconGroup}`}>
-					<AiOutlineBell
+					<IoMdHome
 						className={styles.icon}
-						style={{ fontSize: '24px', cursor: 'pointer' }}
+						style={{ fontSize: '24px' }}
+						onClick={handldHome}
 					/>
-					<AiOutlineUser
-						className={styles.icon}
-						style={{ fontSize: '24px', cursor: 'pointer' }}
-					/>
-
 					{/* Log Out Button */}
-					<div
-						className={`${styles.icon} ${styles.logout}`}
-						onClick={handleLogout}
-						style={{ display: 'flex', alignItems: 'center', cursor: 'pointer' }}
-					>
+					<div className={`${styles.icon} ${styles.logout}`} onClick={handleLogout}>
 						<span style={{ marginLeft: '5px' }}>Log Out</span>
 						<FiLogOut style={{ fontSize: '24px', marginLeft: '5px' }} />
 					</div>
