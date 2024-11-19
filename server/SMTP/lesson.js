baaf6c80a14dfe11d7510788e506686db2c58f60
Tzu <jf76501@gmail.com>
2024-11-19 10:32:31 +0800

    refactor(SMTP)lesson報名發信樣式調整

--- server/SMTP/lesson.js
+++ server/SMTP/lesson.js
@@ -16,7 +16,23 @@ export const sendOrderConfirmation = async (userMail, lesson) => {
     to: userMail,
     subject: '報名成功通知',
     text: `感謝您報名${lesson.name}！`,
-    html: `<p>感謝您的報名！您的報名詳情如下：</p><p>課程名稱：${lesson.name}</p><p>時間：${lesson.start_date}</p><p>地點：${lesson.classroom_name} ${lesson.location} </p>`,
+    html: `
+    <!DOCTYPE html>
+        <html>
+        <body>
+            <h1>感謝您的報名！</h1>
+            <p>您的報名詳情如下：</p>
+            <p>課程名稱：${lesson.name}</p><p>時間：${lesson.start_date}</p>
+            <p>地點：${lesson.classroom_name} ${lesson.location} </p>
+            <a href="http://localhost:3000/lesson/${lesson.id}" style="color: #ffffff;
+        background-color: #fe6f67;
+        padding: 10px;
+        text-decoration: none;
+        text-align: center;
+        align-content: center">了解更多</a>
+        </body>
+        </html>
+   `,
   }
   console.log('Mail options:', mailOptions)
   try {
